ce589622f1b50358c7dd9c9236fd157bc060403c
gaurav <gaurav@ggvaidya.com>
2013-10-01 15:33:47 -0500

    Added tests for DOI::supported?, and fixed a few bugs.

--- lib/biburi/driver/doi.rb
+++ lib/biburi/driver/doi.rb
@@ -5,16 +5,21 @@ require 'json'
 module BibURI::Driver::DOI
   include BibURI::Driver
 
+  def self.supported?(id)
+    canonical = BibURI::Driver::DOI::canonical(id)
+    return !(canonical.nil?)
+  end
+
   # The canonical form of a DOI is:
   # http://dx.doi.org/10.1234/abcd-5678
   def self.canonical(id)
     # Might be 'doi:...'
-    match = id.match(/^doi:(.*)$/)
+    match = id.match(/^doi:(.*)$/i)
     if match
         return "http://dx.doi.org/" + match[1]
     end
 
-    match = id.match(/^http:\/\/dx\.doi\.org\/(.*)$/)
+    match = id.match(/^http:\/\/dx\.doi\.org\/(.*)$/i)
     if match
         return "http://dx.doi.org/" + match[1]
     end
--- spec/doi_spec.rb
+++ spec/doi_spec.rb
@@ -1,6 +1,34 @@
 require_relative 'spec_helper'
 
 describe BibURI::Driver::DOI do
+    describe "supported" do
+        it "should support real DOIs" do
+            real_dois = [
+                "doi:10.1111/j.1529-8817.2010.00939.x",
+                "doi:10.1038/171737a0",
+                "DOI:10.1038/171737A0",
+                "http://dx.doi.org/doi:10.1038/171737a0"
+            ]
+
+            real_dois.each do |doi|
+                expect(BibURI::Driver::DOI::supported?(doi)).to be_true;
+            end
+        end
+
+        it "should not support incorrect DOIs" do
+            incorrect_dois = [
+                "10.1111/j.1529-8817.2010.00939.x",
+                "dxoi:10.1038/171737a0",
+                "https://dx.doi.org/doi:10.1038/171737a0"
+            ]
+
+            incorrect_dois.each do |doi|
+                expect(BibURI::Driver::DOI::supported?(doi)).to be_false;
+            end
+
+        end
+    end
+
     describe "canonical" do
         it "should canonicalize DOIs correctly" do
             canonicalize = {
