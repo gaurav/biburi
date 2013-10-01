c5b04cade7841411b13e25857e639ad254d48314
gaurav <gaurav@ggvaidya.com>
2013-10-01 16:44:53 -0500

    Made DOI actually work.

--- lib/biburi/driver/doi.rb
+++ lib/biburi/driver/doi.rb
@@ -1,5 +1,6 @@
 require 'biburi'
 require 'biburi/driver'
+require 'net/http'
 require 'json'
 
 module BibURI::Driver::DOI
@@ -31,7 +32,44 @@ module BibURI::Driver::DOI
   # Returns a dictionary of parsed values with
   # BibTeX names by looking up the provided id.
   def self.lookup(id)
-    raise NotImplementedError
-  end
+    canonical_id = canonical(id)
+    if canonical_id.nil? then
+        return nil
+    end
 
+    crossref_url = "http://search.crossref.org/dois?" + 
+        URI.encode_www_form([["q", canonical_id]])
+
+    content = Net::HTTP.get(URI(crossref_url))
+    as_json = JSON.parse(content)
+
+    if as_json.length == 0 then
+        # No values returned? Returned nil so that the search
+        # can continue.
+        return nil
+
+    elsif as_json.length > 1 then
+        # More than one search result? Report all the DOI matches
+        # as 'title'.
+        all_dois = []
+        as_json.each do |doi|
+            all_dois.push(doi['doi'])
+        end
+
+        return {
+            "title" => "#{as_json.length()} DOIs returned: " + all_dois.join(', ')
+        }
+    end
+
+    # A single search result? Make sure the DOI matches, then report it.
+    match = as_json[0]
+    unless match['doi'].downcase == canonical_id.downcase then
+        return nil
+    end
+
+    return {
+        "title" => match["fullCitation"],
+        "year" => match["year"]
+    }
+  end
 end
--- spec/doi_spec.rb
+++ spec/doi_spec.rb
@@ -42,4 +42,28 @@ describe BibURI::Driver::DOI do
             end
         end
     end
+
+    describe "lookup" do
+        it "should be able to download information on some DOIs" do
+            queries = {
+                "doi:10.1038/171737a0" => {
+                    "title" => "J. D. WATSON, F. H. C. CRICK, 1953, 'Molecular Structure of Nucleic Acids: A Structure for Deoxyribose Nucleic Acid', <i>Nature</i>, vol. 171, no. 4356, pp. 737-738",
+                    "year" => "1953"
+                },
+                "http://dx.doi.org/10.1111/j.1096-0031.2010.00329.x" => {
+                    "title" => "Gaurav Vaidya, David J. Lohman, Rudolf Meier, 2011, 'SequenceMatrix: concatenation software for the fast assembly of multi-gene datasets with character set and codon information', <i>Cladistics</i>, vol. 27, no. 2, pp. 171-180",
+                    "year" => "2011"
+                },
+                "doi:Hullo there" => {
+                    "title" => "20 DOIs returned: http://dx.doi.org/10.7554/elife.00459.014, http://dx.doi.org/10.7554/elife.00190.025, http://dx.doi.org/10.7554/elife.00178.026, http://dx.doi.org/10.7554/elife.00288.003, http://dx.doi.org/10.7554/elife.00288.022, http://dx.doi.org/10.7554/elife.00288.031, http://dx.doi.org/10.7554/elife.00288.040, http://dx.doi.org/10.7554/elife.00288.041, http://dx.doi.org/10.7554/elife.00362.013, http://dx.doi.org/10.7554/elife.00005.020, http://dx.doi.org/10.7554/elife.00260.015, http://dx.doi.org/10.7554/elife.00426.019, http://dx.doi.org/10.7554/elife.00248.007, http://dx.doi.org/10.7554/elife.00231.018, http://dx.doi.org/10.7554/elife.00327.010, http://dx.doi.org/10.7554/elife.00312.018, http://dx.doi.org/10.7554/elife.00354.003, http://dx.doi.org/10.7554/elife.00354.008, http://dx.doi.org/10.7554/elife.00354.015, http://dx.doi.org/10.7554/elife.00067.017"
+                }
+            }
+
+            queries.keys.each do |query|
+                result = BibURI::Driver::DOI::lookup(query)
+
+                expect(result).to eq(queries[query])
+            end 
+        end
+    end
 end
